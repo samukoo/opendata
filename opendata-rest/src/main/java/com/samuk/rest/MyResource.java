49570996e28eb7d627c759701ee57e6e52ae26c8
unknown <kotissa@UBI-69.ubisecure.ubi>
2015-05-18 23:02:36 +0300

    del

--- opendata-rest/src/main/java/com/samuk/rest/MyResource.java
+++ opendata-rest/src/main/java/com/samuk/rest/MyResource.java
@@ -1,9 +1,15 @@
 package com.samuk.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.samuk.opendata.objectModels.ParkMeter;
+
 
 /**
  */
@@ -12,12 +18,17 @@ public class MyResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Track getIt() {
-        Track track = new Track();
-        
-        track.setAlbum("One");
-        track.setTrack("Nothing else matters");
+    public List<ParkMeter> getIt() {
     	
-    	return track;
+    	ParkMeter pm1 = new ParkMeter();
+    	ParkMeter pm2 = new ParkMeter();
+    	List<ParkMeter>pm = new ArrayList<ParkMeter>();
+    	pm.add(pm1);
+    	pm.add(pm2);
+    	
+    	
+    	
+    	
+    	return pm;
     }
 }
